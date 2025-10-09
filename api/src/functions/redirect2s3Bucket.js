fafa9be2e261b2abfc3d9cccf0516b7b93663b94
Michael Rolli <michael@rollis.ch>
2025-10-09 06:43:07 +0200

    feat: add dynamic routing
    Inspired from this post:
    https://johnnyreilly.com/azure-static-web-apps-dynamic-redirects-azure-functions

---
+++ api/src/functions/redirect2s3Bucket.js
@@ -0,0 +1,42 @@
+const { app } = require("@azure/functions");
+
+const s3BucketBase = "https://zhw-b.s3.cloud.switch.ch/aiub";
+
+app.http("redirect2s3bucket", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    const originalUrl = req.headers["x-ms-original-url"];
+
+    if (originalUrl) {
+      // This URL has been proxied as there was no static file matching it.
+      context.log(`x-ms-original-url: ${originalUrl}`);
+
+      const url = new URL(originalUrl);
+      const path = url.pathname;
+
+      if (path === "/" || path === "") {
+        // no path provided, redirect to index
+        destinationUrl = "/";
+      } else {
+        destinationUrl = s3BucketBase + path;
+      }
+
+      context.log(`Redirecting ${originalUrl} to ${destinationUrl}`);
+
+      context.res = {
+        status: 302,
+        headers: { location: destinationUrl },
+      };
+      return;
+    }
+
+    // No x-ms-original-url header has been set, redirect to index
+    context.res = {
+      status: 302,
+      headers: {
+        location: "/",
+      },
+    };
+  },
+});
