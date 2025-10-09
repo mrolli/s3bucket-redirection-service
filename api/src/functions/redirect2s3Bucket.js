749b5101de348a65ffeff291a969bc476e0bc09b
Michael Rolli <michael@rollis.ch>
2025-10-09 12:42:22 +0200

    refactor: improve execution path

--- api/src/functions/redirect2s3Bucket.js
+++ api/src/functions/redirect2s3Bucket.js
@@ -1,6 +1,7 @@
 const { app } = require("@azure/functions");
 
-const s3BucketLister = "https://code.aiub.unibe.ch/s3_script/aiub_s3_bucket_listing.php"
+const s3BucketLister =
+  "https://code.aiub.unibe.ch/s3_script/aiub_s3_bucket_listing.php";
 const s3BucketBase = "https://zhw-b.s3.cloud.switch.ch/aiub";
 
 /**
@@ -17,31 +18,25 @@ app.http("redirect2s3bucket", {
     // This URL has been proxied as there was no static file matching it.
     context.log(`x-ms-original-url: ${originalUrl}`);
 
+    // By default, if no path is found, redirect to the bucket lister
+    destinationUrl = s3BucketLister;
+
     if (originalUrl) {
       const url = new URL(originalUrl);
       const path = url.pathname;
 
-      if (path === "/" || path === "") {
-        // no path provided, redirect to index
-        destinationUrl = s3BucketLister;
-      } else {
+      if (path !== "/" && path !== "") {
+        // Has path, redirect to the S3 bucket
         destinationUrl = s3BucketBase + path;
       }
-
-      context.log(`Redirecting ${originalUrl} to ${destinationUrl}`);
-
-      return {
-        status: 302,
-        headers: { location: destinationUrl },
-      };
-      return;
     }
 
-    // No x-ms-original-url header has been set, redirect to index
+    context.log(`Redirecting ${originalUrl} to ${destinationUrl}`);
+
     return {
       status: 302,
       headers: {
-        location: s3BucketLister,
+        location: destinationUrl,
       },
     };
   },
